ed40bad0a560fc6742f0dbb3acc6a50c9bff220b
suryaprakashchamoli@gmail.com <suryaprakashchamoli@gmail.com>
2025-02-11 11:59:56 +0530

    git first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+
+function App() {
+
+
+  return (
+    <>
+
+    <h1>App file</h1> 
+     {/* <Header /> */}
+     {/* <Home /> */}
+     {/* <Footer /> */}
+
+      
+
+    </>
+  )
+}
+
+export default App
